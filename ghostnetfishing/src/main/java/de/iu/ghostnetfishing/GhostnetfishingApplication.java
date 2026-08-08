87bac5d78cc3bde483fb02bb0377c8d10525a797
Denis Durakovic <denis.durakovic@iu-study.org>
2026-08-08 02:06:01 +0200

    Initial commit

---
+++ ghostnetfishing/src/main/java/de/iu/ghostnetfishing/GhostnetfishingApplication.java
@@ -0,0 +1,13 @@
+package de.iu.ghostnetfishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostnetfishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostnetfishingApplication.class, args);
+	}
+
+}
---
+++ ghostnetfishing/src/test/java/de/iu/ghostnetfishing/GhostnetfishingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iu.ghostnetfishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostnetfishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
